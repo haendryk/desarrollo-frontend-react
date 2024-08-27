d8ce772c448c5a33040ff17946c232904fad02d6
haendryk <heandryk@gmail.com>
2024-08-27 17:05:14 -0400

    Actualizacion 12

---
+++ src/components/OpenLinks.jsx
@@ -0,0 +1,16 @@
+const OpenLinks = ({ title, url }) => {
+    return (
+        <div>
+            <a
+                className="App-link"
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                {title}
+            </a>
+        </div>
+    );
+};
+
+export default OpenLinks;
